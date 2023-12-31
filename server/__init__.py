0435488db48c24be7ea8487022bb6a70fe40f270
Bao Ngoc Ta <baongoc.ta04@gmail.com>
2023-12-31 17:51:55 -0500

    create connection pool

--- server/__init__.py
+++ server/__init__.py
@@ -1,21 +1,13 @@
 import os
 from flask import Flask, request
+from connection_pool import get_connection
+import db
 
 def create_app():
     app = Flask(__name__)
 
-    connection = psycopg2.connect(database_url)
-    
-    @app.post("/category")
-    def create_category():
-        data = request.get_json()
-        category_name = data["category_name"]
-        with connection:
-            with connection.cursor() as cursor:
-                cursor.execute(CREATE_CATEGORY_TABLE)
-                cursor.execute(INSERT_CATEGORY_RETURN_ID, (category_name,))
-                category_id = cursor.fetchone()[0]
-        return {"category_id": category_id, "message": f"Category {category_name} created."}, 201
+    with get_connection() as connection:
+        db.create_tables(connection)
     
 
     @app.route('/')
